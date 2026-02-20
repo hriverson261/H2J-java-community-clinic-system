a95e2d52b5c74826a4d8a7f0966fc1782335ee8b
jamiird261-collab <jamiird261@gmail.com>
2026-02-20 16:00:49 -0500

    "created member comfirmation boolean system class"

--- src/main/java/org/codedifferently/GymSystem.java
+++ src/main/java/org/codedifferently/GymSystem.java
@@ -2,7 +2,13 @@ package org.codedifferently;
 
 public class GymSystem {
 
-        private int memberCount = 0;
-        private int guestCount = 0;
-        private int completedAppointments = 0;
-        private final String gymName = "H2J GYM";
+    private int memberCount = 0;
+    private int guestCount = 0;
+    private int completedAppointments = 0;
+    private final String gymName = "H2J GYM";
+
+    public void recordVisit(boolean isMember) {
+        if (isMember) memberCount++;
+        else guestCount++;
+    }
+}
