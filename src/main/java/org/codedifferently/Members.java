ad085ec11e89d3c465eeef359115d07bc6c931a6
James Kollilon Barclay III <jkbarclay261@gmail.com>
2026-02-20 15:55:25 -0500

    "Members class and declared variables "

--- src/main/java/org/codedifferently/Members.java
+++ src/main/java/org/codedifferently/Members.java
@@ -1,8 +1,28 @@
 package org.codedifferently;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Members {
 
-    String name;
-    String email;
+    private ArrayList<Member> memberList = new ArrayList<>(); // Array
+    private final String gymName = "H2J GYM"; //Gym name declared and initialized
+
+    //  declared values
+    static class Member {
+        String name;
+        int age;
+        String gender;
+        String email;
+
+        Member(String name, int age, String gender, String email) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.email = email;
+        }
+
+    }
+
 
 }
